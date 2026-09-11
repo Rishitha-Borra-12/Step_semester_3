c5bdefb3438431f896ca99abeb6132fe1bd2e007
Your Name <rb2409@srmist.edu.in>
2026-09-11 16:42:05 +0530

    Add session 3

---
+++ Step_semester_3/src/W3_ControlFlow/Class_Problems/HostelRoom.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class HostelRoom {
+    String roomNo;
+    int occupied;
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        HostelRoom roomA = new HostelRoom();
+        HostelRoom roomB = new HostelRoom();
+
+        roomA.roomNo = sc.nextLine();
+        roomB.roomNo = sc.nextLine();
+
+        roomA.occupied++;
+        roomA.occupied++;
+        roomA.occupied++;
+
+        System.out.println(roomA.roomNo + " occupied: " + roomA.occupied);
+        System.out.println(roomB.roomNo + " occupied: " + roomB.occupied);
+    }
+}
