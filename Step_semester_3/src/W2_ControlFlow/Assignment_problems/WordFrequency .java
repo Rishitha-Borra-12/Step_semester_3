e6e79a8597dde62ea9c8057ef97f94d76d9b8594
Your Name <rb2409@srmist.edu.in>
2026-09-11 11:32:53 +0530

    Add session 2 problems

---
+++ Step_semester_3/src/W2_ControlFlow/Assignment_problems/WordFrequency .java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.List;
+
+public class WordFrequency {
+
+    static void printFilteredWordFrequency(String feedback) {
+
+        feedback = feedback.toLowerCase();
+
+        feedback = feedback.replace(".", "");
+        feedback = feedback.replace(",", "");
+
+        String[] words = feedback.split("\\s+");
+
+        HashSet<String> stopWords = new HashSet<>();
+
+        stopWords.add("the");
+        stopWords.add("was");
+        stopWords.add("and");
+        stopWords.add("a");
+        stopWords.add("is");
+        stopWords.add("of");
+        stopWords.add("in");
+
+        HashMap<String, Integer> frequency = new HashMap<>();
+
+        for (String word : words) {
+
+            if (stopWords.contains(word)) {
+                continue;
+            }
+
+            if (frequency.containsKey(word)) {
+                frequency.put(word, frequency.get(word) + 1);
+            } else {
+                frequency.put(word, 1);
+            }
+        }
+
+        List<Map.Entry<String, Integer>> list =
+                new ArrayList<>(frequency.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        for (Map.Entry<String, Integer> entry : list) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String feedback = sc.nextLine();
+
+        printFilteredWordFrequency(feedback);
+    }
+}
